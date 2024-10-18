652868ee957d00bc7a6361f27fbe85b6aa25a224
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2024-10-18 12:23:31 -0300

    Atualizar o P8.c

--- P8.c
+++ P8.c
@@ -1,38 +1,32 @@
 #include<stdio.h>
 
 // Questao 1) IN: 5  OUT: fib(5)=14 calls=8
-
-int *fib_and_calls(int x, int *calls)
+ 
+int fib_and_calls(int x, int *calls)
 {
+    (*calls)++;
     if(x==0)
-     {
-        *calls++;
         return 0;
-     }
     else if (x==1)
-    {
-        *calls++;
         return 1;
-    }
-    return fib_and_calls(x-1, calls) + fib_and_calls(x-2, calls);
+    return fib_n_call(x-1, calls) + fib(x-2, calls);
 }
 
 int main()
 {
-    int N, x, calls=0;
+    int N, x, result, calls=-1;
 
     scanf("%d", &N);
 
-    if(!(0<N<40))
-        exit(0);
-
+    if(N<1 || N>40)
+        return 0;
+    
     while(N>0)
     {
         scanf("%d", &x);
-        int y=x;
-        fib_and_calls(&x, &calls);
-        printf("fib(%d)=%d  calls=%d\n", y, x, calls);
+        result = fib_and_calls(x, &calls);
+        printf("fib(%d)=%d  calls=%d\n", x, result, calls);
         N--;
     }
+    return 0;
 }
-
