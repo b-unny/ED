5ee6b1da94461af0fb87ebf4ac5d110f2197f3ff
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2024-11-13 22:03:18 -0300

    Update avl_tree.c

--- avl_tree.c
+++ avl_tree.c
@@ -1,1 +1,27 @@
-#include<
+#include<avl.h>
+
+typedef struct AVLnode{
+  int info;
+  int altura = 0; 
+  struct AVLnode *esq;
+  struct AVLnode *dir;
+} avl;
+
+avl *novoNo(int info)
+{
+  alv *novo = (avl*)malloc(sizeof(avl));
+  if(novo==NULL)
+  {
+    printf("Erro de alocação para %d\n", info);
+    exit(1);
+  }
+  novo->info = info;
+  novo->dir  = NULL;
+  novo->esq  = NULL;
+  return novo;
+}
+
+avl *adicionar(avl *raiz, int info)
+{
+  
+}
