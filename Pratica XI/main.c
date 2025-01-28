83c7d62e04ffc708194c33c45f8bd538ecda334a
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2025-01-28 00:55:07 -0300

    Atualizar o main.c
    (

--- Pratica XI/main.c
+++ Pratica XI/main.c
@@ -1,3 +1,35 @@
+#include<stdlib.h>
 #include<stdio.h>
-#include<time.h>
-#include"sort.h"
+#include"lib.h"
+
+int main( )
+{
+    while(1)
+    {
+        int t;
+        printf("Tamanho (0 para sair): ");
+        scanf("%d", &t);
+        
+        if(t==0) break;
+
+        Vetor *v=criarVet(t);
+        
+        preencherVet(v);
+        //verificar(v->vet, t);
+
+        calcTempo(v, bubbleSort, &v->bubble);
+
+        calcTempo(v, quickSort, &v->quick);
+  
+        calcTempo(v, heapSort,&v->heap);
+
+        calcTempo(v, shellSort,&v->shell);
+
+        imprimirTempo(v);
+
+        free(v->vet);
+        free(v);
+        printf("\n");
+    }
+    return 0;
+}
